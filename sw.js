d9f575e3938b67913d908c47174b740f20ba1369
Witalis Domitrz <witekdomitrz@gmail.com>
2025-08-09 00:57:17 +0100

    Formatted

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
-const CACHE = 'pwa-cache-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = "pwa-cache-v1";
+const FILES = ["/", "/index.html", "/manifest.json"];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
-  );
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
